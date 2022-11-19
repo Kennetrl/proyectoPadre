ef3cee0ebce3e6bf785ffff3fc70b672454a201a
Kennet Rodriguez <kennet_rl@hotmail.com>
2022-11-19 14:03:27 -0500

    Estructura lista para creacion masiva

--- puertasScript.js
+++ puertasScript.js
@@ -1,5 +1,8 @@
 //Añadir elementos
+const cardsContainers = document.querySelector('.cards-container');
 const productList = [];
+
+//Lisa de los elementos
 productList.push({
     name: 'Puerta1',
     price: 120,
@@ -15,37 +18,58 @@ productList.push({
     price: 100,
     image: './puertasImagenes/3puerta.jpeg',
 });
+productList.push({
+    name: 'Puerta3',
+    price: 100,
+    image: './puertasImagenes/3puerta.jpeg',
+});
+productList.push({
+    name: 'Puerta3',
+    price: 100,
+    image: './puertasImagenes/3puerta.jpeg',
+});
+productList.push({
+    name: 'Puerta3',
+    price: 100,
+    image: './puertasImagenes/3puerta.jpeg',
+});
+//Función para mostrar las lista
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+                
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+                
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+                
+        const productInfoDiv = document.createElement('div');
+                
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+                
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+                
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
 
-/*
-    <div class="unidad">
-                <img src="./puertasImagenes/10puerta.jpeg" alt="Imagen Puerta" class="imagenesPuertas">
-                <h3>PUERTA EJEMPLO 1</h3>
-                <div class="ordenar">
-                    <p>$210.00</p>
-                    <img src="./icons/whatsapp.png" alt="whatsappIcon" class="whatsappIcon">
-                </div>
-            </div>
-    */
-for(product of productList){
-    const productCard = document.createElement('div');
-    productCard.classList.add('unidad');
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src', product.image);
-
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('ordenar');
-
-    const productInfoDiv = document.createElement('div');
-
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-
-    const productName = document.createElement('p');
-    productPrice.innerText = product.name;
-
-    productInfo.appendChild(productInfoDiv);
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
-
-
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/whatsapp.png');
+    
+        productInfoFigure.appendChild(productImgCart);
+                
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        
+         cardsContainers.appendChild(productCard);
+    
+    }
 }
+renderProducts(productList);
